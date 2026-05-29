4a41d26f14035684687f215b053b44f5b20ee1cb
Ryan Palmer <ryanpalmercs@gmail.com>
2026-05-29 10:10:01 -0500

    Attach modal to App.jsx so it's cross the whole site and animate open and close

--- src/api/feedbackApi.js
+++ src/api/feedbackApi.js
@@ -1,4 +1,11 @@
-import api from "./api";
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'X-API-KEY': import.meta.env.VITE_API_KEY
+    }
+})
 
 export function submitFeedback(data) {
     return api.post("/feedback", data)
